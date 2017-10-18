2f2039b332723457ea5d3c895b2277d78b5b6991
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-18 14:12:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+def display_board(board)
+    puts "   | #{board[1]} |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "  
+end
+
+print display_board(board)
